d9edb59864b317cd4d2b07c93e5f6c72d926e249
nhtoan611 <nhtoan611@gmail.com>
2017-07-24 10:45:01 +0700

    advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,16 +6,27 @@ class SessionsController < ApplicationController
     user = User.find_by email: session[:email].downcase
 
     if user && user.authenticate(session[:password])
-      log_in user
-      redirect_to user
+      login_success user
     else
-      flash.now[:danger] = t "error.login_fail"
-      render :new
+      login_fail
     end
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
+  end
+
+  private
+
+  def login_success user
+    log_in user
+    params[:session][:remember_me] == "1" ? remember(user) : forget(user)
+    redirect_to user
+  end
+
+  def login_fail
+    flash.now[:danger] = t "error.login_fail"
+    render :new
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  attr_reader :remember_token
+
   has_secure_password
 
-  validates :password, presence: true, length: {minimum: 6}
+  validates :password, presence: true, length: {minimum: 6}, allow_nil: true
   validates :name, presence: true, length: {maximum: 50}
   validates :email, presence: true, length: {maximum: 255},
     format: {with: VALID_EMAIL_REGEX},
@@ -20,6 +22,24 @@ class User < ApplicationRecord
         end
       BCrypt::Password.create string, cost: cost
     end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    @remember_token = User.new_token
+    update_attributes remember_digest: User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    return false if remember_digest.blank?
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
+
+  def forget
+    update_attributes remember_digest: nil
   end
 
   private
